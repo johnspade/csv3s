ca01d11a4068cf5a96bb3f7024eb617c6994c36b
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2025-02-27 23:53:56 +0100

    Update magnolia to 1.3.16 (#101)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,7 +4,7 @@ object Dependencies {
   object V {
     val zio       = "2.1.16"
     val zioParser = "0.1.11"
-    val magnolia  = "1.3.15"
+    val magnolia  = "1.3.16"
   }
 
   val zio        = "dev.zio"                      %% "zio"          % V.zio
